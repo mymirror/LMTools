5ff65ba8a9daa3b8df20f646b35d021ecee2590d
ponted <ponted@ponteddeMacBook-Pro.local>
2018-11-26 15:38:29 +0800

    subject : add

---
+++ LMTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMTools
+//
+//  Created by ponted on 2018/11/26.
+//  Copyright © 2018 Shenzhen Blood Link Medical Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LMTools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LMTools
+//
+//  Created by ponted on 2018/11/26.
+//  Copyright © 2018 Shenzhen Blood Link Medical Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
